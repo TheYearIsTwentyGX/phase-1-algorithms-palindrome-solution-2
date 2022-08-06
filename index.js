b1bd883bceb69da6917c37a9eb25a1a6d1b1c1a8
Dylan Hays <twentygx@outlook.com>
2022-08-06 15:47:14 -0500

    commit tree f1611e3dafc864ec0da220079ab530bc0bcec31a

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  for (let i = 0; i < word.length/2; i++) {
+    if (word[i] != word[word.length - 1 - i])
+      return false;
+  }
+  return true;
 }
 
 /* 
-  Add your pseudocode here
+  Loop for half the length of the word
+    Round down since we won't need to check the center letter of a word of an odd length
+  Each loop, check if (character i from start of word) === (character i from end of word)
+  If it fails that test, then return false
 */
 
 /*
-  Add written explanation of your solution here
+  Get half the length of the array (rounding down)
+  Loop for half the length, storing how many loops we've gone through as 'i'
+    if the character 'i' characters away from the start of the word is not the same as the character 'i' characters away from the end of the word, then return false
+  if you finish all the loops, return true
 */
 
 // You can run `node index.js` to view these console logs
